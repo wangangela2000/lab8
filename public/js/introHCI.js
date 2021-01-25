54bf63beae854fe0b9a9dc7a4afa2a2c88949709
wangangela2000 <wangangela2000@gmail.com>
2021-01-24 22:42:44 -0800

    update

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,8 +10,8 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Page ready");
- 	// initCamera();
- 	// initMap();
+ 	initCamera();
+ 	initMap();
  	initGestures();
  	initRSVPForm();
 }
@@ -19,9 +19,31 @@ function initializePage() {
 // init jQuery gestures  
 function initGestures() {
 	// add gestures listener here
+	$(function(){
+		$(".judge-img").bind("taphold", tapholdHandler);
+
+		function tapholdHandler(event){
+			var targetIDPrefix = event.target.id;
+			console.log("got prefix:" + targetIDPrefix);
+
+			$("#" + targetIDPrefix + "-bio").show();
+		}
+	});
 }
 
 // init RSVP form submit listener
 function initRSVPForm() {
   // add your code here
+  $('#rsvpForm').submit(function(e) {
+	  e.preventDefault();
+	  console.log("submitting form...");
+	  var rsvpEmail = $('#rsvpEmail').val();
+	  // send the POST
+	  $.post('addRSVP', { rsvpEmail: rsvpEmail }, postCallback);
+  });
+
+  function postCallback(res) {
+	  alert("RSVP form successfully submitted!");
+	  $('#rsvpEmail').val(''); // Clear form
+  }
 }
--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,14 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'eKiym9DiSs86ngYbiBGm43HSY1yBEEFl';
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+    center: [32.88, -117.236],
+    layers: L.mapquest.tileLayer('map'),
+	zoom: 12,
+	zoomControl: false
+	});
+
+	L.marker([32.88, -117.236]).addTo(map);
 }
